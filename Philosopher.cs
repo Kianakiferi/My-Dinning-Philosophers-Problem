1ddaebb7b94a6479e979ea31bff22c183358604e
Kianakiferi <42063176+Kianakiferi@users.noreply.github.com>
2021-04-03 21:47:54 +0800

    First Commit

---
+++ Philosopher.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Threading;
+
+namespace PhilosophersConsoleApp.Model
+{
+	class Philosopher
+	{
+		public const int MAX_THINKING_TIME = 2000;
+		public const int MAX_EATING_TIME = 500;
+
+		public const int MAX_WATING_TIME = 4000;
+
+		public const int MAX_CHOPSTICKS = 5;
+
+		private Random Random = new();
+		private static int GenericId = 0;
+
+		public int Id;
+		public string Name;
+		public string PrintOffset;
+
+		public int LeftMutexIndex;
+		public Mutex LeftMutex;
+
+		public int RightMutexIndex;
+		public Mutex RightMutex;
+
+		#region CTOR
+		public Philosopher(string name)
+		{
+			Id = GenericId;
+			GenericId++;
+
+			Name = name;
+
+			if (Id is > 0 and < MAX_CHOPSTICKS)
+			{
+				LeftMutexIndex = Id - 1;
+			}
+			else
+			{
+				LeftMutexIndex = Math.Abs(Id - (MAX_CHOPSTICKS - 1));
+			}
+
+			RightMutexIndex = Id;
+
+			for (int i = 0; i < Id; i++)
+			{
+				PrintOffset += "			";
+			}
+
+			Console.WriteLine($"Thread {Id} Now Online");
+		}
+
+		public void InitSemaphores(Mutex leftMutex, Mutex rightMutex)
+		{
+			this.LeftMutex = leftMutex;
+			this.RightMutex = rightMutex;
+		}
+		#endregion
+
+		#region Daily Life
+		public void Think()
+		{
+			Console.WriteLine($"{PrintOffset}| {this.Id}: Thinking		|");
+			Thread.Sleep(Random.Next(MAX_THINKING_TIME));
+		}
+
+		public void PickLeft()
+		{
+			Console.WriteLine($"{PrintOffset}| {this.Id}: Waiting		|");
+
+			if (LeftMutex.WaitOne(MAX_WATING_TIME))
+			{
+				Console.WriteLine($"{PrintOffset}| {this.Id}: Got left {LeftMutexIndex}		|");
+			}
+			else
+			{
+				Console.WriteLine($"{PrintOffset}| {this.Id}: Starved to death	|");
+			}
+		}
+
+		public void PickRight()
+		{
+			if (RightMutex.WaitOne(MAX_WATING_TIME))
+			{
+				Console.WriteLine($"{PrintOffset}| {this.Id}: Got right {RightMutexIndex}	|");
+			}
+			else
+			{
+				Console.WriteLine($"{PrintOffset}| {this.Id}: Starved to death	|");
+			}
+		}
+
+		public void Eat()
+		{
+			Console.WriteLine($"{PrintOffset}| {this.Id}: Eating		|");
+			Thread.Sleep(Random.Next(MAX_EATING_TIME));
+		}
+
+		public void Release()
+		{
+			try
+			{
+				Console.WriteLine($"{PrintOffset}| {Id}: Released left {LeftMutexIndex}	|");
+				LeftMutex.ReleaseMutex();
+			}
+			catch (Exception)
+			{
+
+			}
+
+			try
+			{
+				Console.WriteLine($"{PrintOffset}| {Id}: Released right {RightMutexIndex}	|");
+				RightMutex.ReleaseMutex();
+			}
+			catch (Exception)
+			{
+
+			}
+		}
+		#endregion
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using PhilosophersConsoleApp.Model;
+
+namespace PhilosophersConsoleApp
+{
+	class Program
+	{
+		public static readonly int DAYTIME = 2;
+
+		private static Mutex[] _mutex;
+
+		private static void SetTable()
+		{
+			_mutex = new Mutex[5]
+			{
+				new Mutex(),
+				new Mutex(),
+				new Mutex(),
+				new Mutex(),
+				new Mutex(),
+			};
+		}
+
+		static void Main(string[] args)
+		{
+			// Chopsticks:  4 0 1 2 3 4
+			// Philosophers: 0 1 2 3 4
+
+			SetTable();
+			DoDailyLife();
+		}
+
+		private static void DoDailyLife()
+		{
+			// Invite philosophers
+			List<Model.Philosopher> customers = new List<Model.Philosopher>
+			{
+				new Philosopher("Aatrox"),
+				new Philosopher("Aurelion Sol"),
+				new Philosopher("Pantheon"),
+				new Philosopher("Ornn"),
+				new Philosopher("Volibear"),
+			};
+
+			// Invite them to sitting and having dinner
+			foreach (var philosopher in customers)
+			{
+				// Distributing the cutlery
+				philosopher.InitSemaphores
+				(
+					_mutex[philosopher.LeftMutexIndex],
+					_mutex[philosopher.RightMutexIndex]
+				);
+
+				Thread dailyLife = new Thread(new ParameterizedThreadStart(DailyLife));
+				dailyLife.Start(philosopher);
+			}
+		}
+
+		private static void DailyLife(object item)
+		{
+			if (item is Model.Philosopher)
+			{
+				var philosopher = item as Model.Philosopher;
+				
+				for (int i = 0; i < DAYTIME; i++)
+				{
+					philosopher.PickLeft();
+					philosopher.PickRight();
+					philosopher.Eat();
+					philosopher.Release();
+					philosopher.Think();
+				}
+
+				Console.WriteLine($"{philosopher.PrintOffset}| Thread {philosopher.Id} Finished	|");
+			}
+		}
+
+
+	}
+}
